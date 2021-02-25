a4081888ad1c0abf30180b503dd08bff149cf5a5
Jacob Walker <jacobdawalker@gmail.com>
2021-02-24 23:42:31 -0500

    Added Draw Card Script and Json Tool Script

---
+++ DragonBallSuper/Assets/Scripts/JsonDataTool.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class JsonDataTool 
+{ 
+    public static string GetJsonFromFile(string path)
+    {
+        string str = File.ReadAllText(Application.dataPath + path, Encoding.GetEncoding("UTF-8"));
+        if (str == null) Debug.LogError("Target resource not found:" + path);
+        return str;
+    }
+}
