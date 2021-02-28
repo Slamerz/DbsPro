ca97ae318c88bc3e5eec053ebc85b230c95880af
Jacob Walker <jacobdawalker@gmail.com>
2021-02-28 11:25:17 -0500

    Card Dynamically pulls image from file system

--- DragonBallSuper/Assets/Scripts/CardDetails.cs
+++ DragonBallSuper/Assets/Scripts/CardDetails.cs
@@ -4,27 +4,45 @@
 // MVID: ABA70E52-8491-417E-BCC4-DE60A04F1D86
 // Assembly location: C:\Users\jacob\RiderProjects\DragonBallSuperOnline\bin\DragonBallSuper_Data\Managed\Assembly-CSharp.dll
 
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDetails : MonoBehaviour
 {
-  public GameObject Canvas;
-  public GameObject ZoomedInCard;
-  private GameObject zoomCard;
-  public GameObject Parent;
+    /*  public GameObject Canvas;
+      public GameObject ZoomedInCard;
+      private GameObject zoomCard;
+      public GameObject Parent;
 
-  public void Awake()
-  {
-    this.Canvas = GameObject.Find("Main_Canvas");
-    this.Parent = GameObject.Find("Card_Details");
-  }
+      public void Awake()
+      {
+        this.Canvas = GameObject.Find("Main-Canvas");
+        this.Parent = GameObject.Find("Card-Zoom");
+      }
 
-  public void onHoverEnter()
-  {
-    this.zoomCard = Object.Instantiate<GameObject>(this.ZoomedInCard, (Vector3) new Vector2(0.0f, 0.0f), Quaternion.identity);
-    this.zoomCard.transform.SetParent(this.Parent.transform, false);
-    this.zoomCard.GetComponent<RectTransform>().sizeDelta = new Vector2(260f, 363f);
-  }
+      public void onHoverEnter()
+      {
+        this.zoomCard = Object.Instantiate<GameObject>(this.ZoomedInCard, (Vector3) new Vector2(0.0f, 0.0f), Quaternion.identity);
+        this.zoomCard.transform.SetParent(this.Parent.transform, false);
+        this.zoomCard.GetComponent<RectTransform>().sizeDelta = new Vector2(260f, 363f);
+      }
 
-  public void OnHoverExit() => Object.Destroy((Object) this.zoomCard);
+      public void OnHoverExit() => Object.Destroy((Object) this.zoomCard);*/
+
+    public void Start()
+    {
+        byte[] fileData;
+
+        var filePath = Application.dataPath + "/42ea51ee-4e38-421c-a52d-87b0c410144b.png";
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            Texture2D texture = new Texture2D(0, 0, TextureFormat.RGB24, false);
+            texture.LoadImage(fileData);
+            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, 260, 363), new Vector2(0, 0));
+            gameObject.GetComponent<Image>().sprite = sprite;
+        }
+        
+     }
 }
