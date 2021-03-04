61d5063e7d604a20f71b63c5483e143aeb15a545
Jacob Walker <jacobdawalker@gmail.com>
2021-03-04 00:08:01 -0500

    Zoom in on cards

--- DragonBallSuper/Assets/Scripts/CardDetails.cs
+++ DragonBallSuper/Assets/Scripts/CardDetails.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
+using Object = System.Object;
 
 public class CardDetails : MonoBehaviour
 {
@@ -25,8 +26,21 @@ public class CardDetails : MonoBehaviour
 
       public void OnHoverExit() => Object.Destroy((Object) this.zoomCard);*/
 
+    public GameObject ZoomedInCard;
+    public GameObject ZoomCardZone;
+
     public void Start()
     {
-        
+        ZoomCardZone = GameObject.Find("Card-Zoom");
     }
+
+
+    public void onHoverEnter()
+    {
+        ZoomedInCard = Instantiate(gameObject, new Vector2(0, 0), Quaternion.identity);
+        ZoomedInCard.transform.SetParent(ZoomCardZone.transform, false);
+        ZoomedInCard.GetComponent<RectTransform>().sizeDelta = new Vector2(88, 122);
+    }
+
+    public void onHoverExit() => Destroy(ZoomedInCard);
 }
